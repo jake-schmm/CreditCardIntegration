242b9635c1bce1873076811fd1ac522c842347ca
jake-schmm <67937281+jake-schmm@users.noreply.github.com>
2022-03-18 14:32:28 -0400

    Add products.php

---
+++ products.php
@@ -0,0 +1,30 @@
+<?php
+    $products = array(
+        "product1" => array(
+            "title" => "My Amazing Product 1", 
+            "price" => 50,
+            "features" => array("feature 1", "feature 2", "feature 3")
+        ),
+        "product2" => array(
+            "title" => "My Amazing Product 2", 
+            "price" => 50,
+            "features" => array("feature 1", "feature 2", "feature 3")
+        ),
+        "product3" => array(
+            "title" => "My Amazing Product 3", 
+            "price" => 50,
+            "features" => array("feature 1", "feature 2", "feature 3")
+        ),
+        "product4" => array(
+            "title" => "My Amazing Product 4", 
+            "price" => 50,
+            "features" => array("feature 1", "feature 2", "feature 3")
+        ),
+        "product5" => array(
+            "title" => "My Amazing Product 5",
+            "price" => 50,
+            "features" => array("feature 1", "feature 2", "feature 3")
+        )
+    );
+?>
+    
